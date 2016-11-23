6a75560a7c650618209b2a079bc3a8dcacba0c45
Marco DiPlacido <marcodi@microsoft.com>
2016-11-23 13:42:51 -0500

    simple binding component

---
+++ src/app/posts/simple-binding/simple-binding.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-binding',
+  templateUrl: './simple-binding.component.html',
+  styleUrls: ['./simple-binding.component.css']
+})
+export class SimpleBindingComponent implements OnInit {
+  counter: number = 0;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  increment() {
+    this.counter++;
+  }
+
+  get square(): number {
+    return this.counter * this.counter;
+  }
+}
